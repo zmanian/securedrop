e04dae5dbce0244d3986d7479ba86bbe6b640cc5
Garrett Robinson <garrett@pressfreedomfoundation.org>
2014-05-23 12:48:26 -0700

    fixing DISPLAY value

--- setup_dev.sh
+++ setup_dev.sh
@@ -207,8 +207,8 @@ EOF
 
 sudo chmod +x /etc/init.d/xvfb
 sudo service xvfb start
-sudo sh -c 'echo "export DISPLAY=:0.0" >> /etc/environment'
-export DISPLAY=:0.0
+sudo sh -c 'echo "export DISPLAY=:1" >> /etc/environment'
+export DISPLAY=:1
 
 echo ""
 echo "Running unit tests... these should all pass!"
